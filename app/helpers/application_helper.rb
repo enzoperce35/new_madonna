b791a75bc433d6b8c1e2ab0b81cb8b90211fe3ec
enzoperce35 <enzoperce35@gmail.com>
2022-02-12 04:54:59 +0800

    added yarlus minor modifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,12 +11,12 @@ module ApplicationHelper
     current_user.username == 'inso'
   end
 
-  def user_is_non_admin
-    current_user.branch.nil?
+  def user_is_admin
+    !current_user.branch.nil?
   end
   
   def branch_name
-    if !user_signed_in? || user_is_non_admin
+    if !user_signed_in? || !user_is_admin
       ''
     else
       current_user.branch.prepend(' - ')
